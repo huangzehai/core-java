34e191f0ba2ed278776093fe6ac8fa8ab1c79c6b
AdamHuang <chaogeek@foxmail.com>
2016-02-23 15:11:07 +0800

    Add JoinTest.java

--- src/main/java/com/hzh/corejava/concurrent/JoinTest.java
+++ src/main/java/com/hzh/corejava/concurrent/JoinTest.java
@@ -2,18 +2,14 @@ package com.hzh.corejava.concurrent;
 
 public class JoinTest {
 	public static void main(String[] args) {
-		Thread thread = new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				try {
-					Thread.sleep(1000);
-					System.out.println("end thread");
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                System.out.println("end thread");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
 		
 		thread.start();
 		try {
