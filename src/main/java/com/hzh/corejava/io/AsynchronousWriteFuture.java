98b2023a04e955a1eee02f9483e9a006fd2c4705
AdamHuang <chaogeek@foxmail.com>
2016-02-24 18:12:47 +0800

    Add NIO demo

---
+++ src/main/java/com/hzh/corejava/io/AsynchronousWriteFuture.java
@@ -0,0 +1,50 @@
+package com.hzh.corejava.io;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.Future;
+import static java.nio.file.StandardOpenOption.WRITE;
+import static java.nio.file.StandardOpenOption.CREATE;
+
+
+/**
+ * Created by Adam on 2016/2/24.
+ */
+public class AsynchronousWriteFuture {
+    public static ByteBuffer getDataBuffer() {
+        String lineSeparator = System.getProperty("line.separator");
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("test");
+        sb.append(lineSeparator);
+
+        String str = sb.toString();
+        Charset cs = Charset.forName("UTF-8");
+        ByteBuffer bb = ByteBuffer.wrap(str.getBytes(cs));
+
+        return bb;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Path path = Paths.get("test.txt");
+
+        try (AsynchronousFileChannel afc = AsynchronousFileChannel.open(path,
+                WRITE, CREATE)) {
+            ByteBuffer dataBuffer = getDataBuffer();
+            Future<Integer> result = afc.write(dataBuffer, 0);
+            while (!result.isDone()) {
+                System.out.println("Sleeping for 1 milliseconds...");
+                Thread.sleep(1);
+            }
+            int writtenBytes = result.get();
+            System.out.format("%s bytes written  to  %s%n", writtenBytes,
+                    path.toAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
