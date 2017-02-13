8037b38327f3e984c10124e35e482568e251dac5
huangzehai <chaogeek@foxmail.com>
2017-02-13 19:32:50 +0800

    Add Blocking Queue Test case

---
+++ src/test/java/com/hzh/corejava/concurrent/BlockingQueueTest.java
@@ -0,0 +1,19 @@
+package com.hzh.corejava.concurrent;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by lenovo on 2017/2/13.
+ */
+public class BlockingQueueTest {
+    @org.junit.Test
+    public void enqueue() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void dequeue() throws Exception {
+
+    }
+
+}
