9f64ecdb8c8e1d9dd84d9519393b11699df103e6
huangzehai <chaogeek@foxmail.com>
2017-02-25 19:20:32 +0800

    Add folk join example

---
+++ src/main/java/com/hzh/corejava/concurrent/MaxWithFJ.java
@@ -0,0 +1,50 @@
+package com.hzh.corejava.concurrent;
+
+import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+
+/**
+ * Created by huangzehai on 2017/2/25.
+ */
+public class MaxWithFJ extends RecursiveAction {
+    private final int threshold;
+    private final SelectMaxProblem problem;
+    public int result;
+
+    public MaxWithFJ(SelectMaxProblem problem, int threshold) {
+        this.problem = problem;
+        this.threshold = threshold;
+    }
+
+    protected void compute() {
+        if (problem.size < threshold)
+            result = problem.solveSequentially();
+        else {
+            int midpoint = problem.size / 2;
+            MaxWithFJ left = new MaxWithFJ(problem.subProblem(0, midpoint), threshold);
+            MaxWithFJ right = new MaxWithFJ(problem.subProblem(midpoint +
+                    1, problem.size), threshold);
+            invokeAll(left, right);
+            result = Math.max(left.result, right.result);
+        }
+    }
+
+    public static void main(String[] args) {
+        int size = 10000000;
+        int[] numbers = new int[size];
+        Random random=new Random();
+        for (int i = 0; i < size; i++) {
+            numbers[i] = random.nextInt(size);
+        }
+        SelectMaxProblem problem = new SelectMaxProblem(numbers,0,size,size);
+        int threshold = 100000;
+        int nThreads = Runtime.getRuntime().availableProcessors();
+        MaxWithFJ mfj = new MaxWithFJ(problem, threshold);
+        ForkJoinPool fjPool = new ForkJoinPool(nThreads);
+
+        fjPool.invoke(mfj);
+        int result = mfj.result;
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/com/hzh/corejava/concurrent/SelectMaxProblem.java
@@ -0,0 +1,33 @@
+package com.hzh.corejava.concurrent;
+
+/**
+ * Created by huangzehai on 2017/2/25.
+ */
+public class SelectMaxProblem {
+    private final int[] numbers;
+    private final int start;
+    private final int end;
+    public final int size;
+
+    public SelectMaxProblem(int[] numbers, int start, int end, int size) {
+        this.numbers = numbers;
+        this.start = start;
+        this.end = end;
+        this.size = size;
+    }
+
+    public int solveSequentially() {
+        int max = Integer.MIN_VALUE;
+        for (int i = start; i < end; i++) {
+            int n = numbers[i];
+            if (n > max)
+                max = n;
+        }
+        return max;
+    }
+
+    public SelectMaxProblem subProblem(int subStart, int subEnd) {
+        return new SelectMaxProblem(this.numbers, start + subStart,
+                start + subEnd, subEnd - subStart);
+    }
+}
