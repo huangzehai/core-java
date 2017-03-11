88d7b8080bad40424bc26e879293d67bd283de23
huangzehai <chaogeek@foxmail.com>
2017-03-11 19:08:22 +0800

    Add FileChannel example

---
+++ src/main/java/com/hzh/corejava/io/BasicIoExample.java
@@ -0,0 +1,18 @@
+package com.hzh.corejava.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by huangzehai on 2017/3/2.
+ */
+public class BasicIoExample {
+    public static void main(String[] args) throws IOException {
+        Path home = Paths.get("/Users/huangzehai/data");
+        Path profile = home.resolve(".test");
+        Files.copy(profile, home.resolve(".test.bak"));
+        Files.isSymbolicLink(profile);
+    }
+}
