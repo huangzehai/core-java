8e4302eb5df6406eefa5876e96ae4b9b18d16a0b
huangzehai <chaogeek@foxmail.com>
2017-10-17 19:55:42 +0800

    Update stream example

--- src/main/java/com/hzh/corejava/function/StreamTest.java
+++ src/main/java/com/hzh/corejava/function/StreamTest.java
@@ -9,7 +9,7 @@ public class StreamTest {
     }
 
     private static void testReduce() {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
         String text = list.stream().map(a -> Integer.toString(a)).reduce((a, b) -> a + "," + b).get();
         System.out.println(text);
     }
