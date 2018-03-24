046d637e3a016e67021fc2b859495cbecfd4f39e
huangzehai <chaogeek@foxmail.com>
2018-03-24 19:08:25 +0800

    添加CompletableFuture 例子

--- src/main/java/com/hzh/corejava/concurrent/completablefuture/CompletableFutureExample.java
+++ src/main/java/com/hzh/corejava/concurrent/completablefuture/CompletableFutureExample.java
@@ -4,12 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.TimeUnit;
 
 public class CompletableFutureExample {
     private static final Logger logger = LoggerFactory.getLogger(CompletableFutureExample.class);
 
     public static void main(String[] args) {
 
+//        runAsync();
+//
+//        completedFutureExample();
+        completeExceptionallyExample();
+    }
+
+    private static void runAsync() {
         CompletableFuture<Void> cf = CompletableFuture.runAsync(() -> {
             try {
                 Thread.sleep(3000);
@@ -24,5 +33,37 @@ public class CompletableFutureExample {
         logger.info("Do something else.");
         cf.join();
         logger.info("end");
+    }
+
+    static void completedFutureExample() {
+        CompletableFuture cf = CompletableFuture.completedFuture("message");
+        logger.info("Is done:" + cf.isDone());
+        logger.info("value now:" + cf.getNow(null));
+    }
+
+    static void completeExceptionallyExample() {
+        CompletableFuture cf = CompletableFuture.completedFuture("message").thenApplyAsync(text -> {
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            return text.toUpperCase();
+            throw new NullPointerException("Test");
+        });
+        CompletableFuture exceptionHandler = cf.handle((s, exception) -> {
+            System.out.println(s);
+            logger.error(exception.toString());
+            return "Have handled exception";
+        });
+//        cf.completeExceptionally(new RuntimeException("completed exceptionally"));
+        System.out.println(cf.isCompletedExceptionally());
+        try {
+            cf.join();
+        } catch (CompletionException ex) { // just for testing
+            ex.printStackTrace();
+        }
+        Object result = exceptionHandler.join();
+        System.out.println(result);
     }
 }
