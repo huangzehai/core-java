3b1327d224bf898102e7c71fc736161cd88a3183
huangzehai <chaogeek@foxmail.com>
2018-03-24 11:17:47 +0800

    添加CompletionService用例

---
+++ src/main/java/com/hzh/corejava/concurrent/completionservice/CompletionServiceExample.java
@@ -0,0 +1,45 @@
+package com.hzh.corejava.concurrent.completionservice;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CompletionServiceExample {
+    // dummy helper to create a List of Callables return a String
+    public static List<Callable<String>> createCallableList() {
+        List<Callable<String>> callables = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            callables.add(new LongRunningTask());
+        }
+        return callables;
+    }
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        CompletionService<String> taskCompletionService = new ExecutorCompletionService<>(executorService);
+        try {
+            List<Callable<String>> callables = createCallableList();
+            for (Callable<String> callable : callables) {
+                taskCompletionService.submit(callable);
+            }
+            for (int i = 0; i < callables.size(); i++) {
+                Future<String> result = taskCompletionService.take();
+                System.out.println(new Date() + ": " + result.get());
+            }
+        } catch (InterruptedException e) {
+            // no real error handling. Don't do this in production!
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            // no real error handling. Don't do this in production!
+            e.printStackTrace();
+        }
+        executorService.shutdown();
+    }
+}
---
+++ src/main/java/com/hzh/corejava/concurrent/completionservice/LongRunningTask.java
@@ -0,0 +1,16 @@
+package com.hzh.corejava.concurrent.completionservice;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+public class LongRunningTask implements Callable<String> {
+    public String call() {
+        // do stuff and return some String
+        try {
+            Thread.sleep(Math.abs(new Random().nextLong() % 5000));
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        return Thread.currentThread().getName();
+    }
+}
