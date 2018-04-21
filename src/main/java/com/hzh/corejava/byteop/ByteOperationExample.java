a3478003e4d0fbf73b4c87ef9dff97155ec89175
huangzehai <chaogeek@foxmail.com>
2018-04-21 12:36:14 +0800

    使用位运算实现整数加法

---
+++ src/main/java/com/hzh/corejava/byteop/ByteOperationExample.java
@@ -0,0 +1,38 @@
+package com.hzh.corejava.byteop;
+
+public class ByteOperationExample {
+    public static void main(String[] args) {
+        System.out.println(add2(9, 233));
+    }
+
+    private static int add(int a, int b) {
+        // 定理1：设a，b为两个二进制数，则a+b = a^b + (a&b)<<1。
+        //进位
+        int jw = a & b;
+        //原位
+        int yw = a ^ b;
+        while (jw != 0) {
+            //临时a：  a^b
+            int ta = yw;
+            //临时b： (a&b)<<1
+            int tb = jw << 1;
+            jw = ta & tb;
+            yw = ta ^ tb;
+        }
+        return yw;
+    }
+
+
+    private static int add2(int a, int b) {
+        // 设a，b为两个二进制数，则a+b = a^b + (a&b)<<1。
+        //进位
+        int jw = (a & b) << 1;
+        //原位
+        int yw = a ^ b;
+        if (jw == 0) {
+            return yw;
+        } else {
+            return add2(yw, jw);
+        }
+    }
+}
